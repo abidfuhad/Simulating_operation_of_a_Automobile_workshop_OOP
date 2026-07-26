cfb06934ea259e0a19c60853d53fa9986da38b5d
nowrin0129 <nowrin0129@gmail.com>
2026-07-26 21:18:04 +0600

    M3_FXML_design

---
+++ src/main/java/com/example/simulating_operation_of_a_automobile_workshop_oop/WorkShopManager/Controller/ReassignTechnicianViewController.java
@@ -0,0 +1,28 @@
+package com.example.simulating_operation_of_a_automobile_workshop_oop.WorkShopManager.Controller;
+
+import javafx.scene.control.*;
+
+public class ReassignTechnicianViewController
+{
+    @javafx.fxml.FXML
+    private TableColumn jobIdTC;
+    @javafx.fxml.FXML
+    private Label titleLabel;
+    @javafx.fxml.FXML
+    private TableColumn vehicleIdTC;
+    @javafx.fxml.FXML
+    private Button saveButtonOnAction;
+    @javafx.fxml.FXML
+    private TableView jobTV;
+    @javafx.fxml.FXML
+    private ComboBox newTechnicianComboBox;
+    @javafx.fxml.FXML
+    private Label newTechinicianLabel;
+    @javafx.fxml.FXML
+    private Button backButtonOnAction;
+    @javafx.fxml.FXML
+    private TableColumn currentTechnicianTC;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
