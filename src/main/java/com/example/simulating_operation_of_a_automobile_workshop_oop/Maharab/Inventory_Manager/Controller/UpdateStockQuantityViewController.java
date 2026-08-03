43bbe7fb72f1b5af7c60f262db27901f6888e49c
meherab-10 <mhasan87601@gmail.com>
2026-08-03 14:01:47 +0600

    file Manage

---
+++ src/main/java/com/example/simulating_operation_of_a_automobile_workshop_oop/Maharab/Inventory_Manager/Controller/UpdateStockQuantityViewController.java
@@ -0,0 +1,27 @@
+package com.example.simulating_operation_of_a_automobile_workshop_oop.Maharab.Inventory_Manager.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class UpdateStockQuantityViewController
+{
+    @javafx.fxml.FXML
+    private ComboBox selectItemCombobox;
+    @javafx.fxml.FXML
+    private TextField quantityAddedField;
+    @javafx.fxml.FXML
+    private TextField quantityRemovedField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void BackButon(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveButton(ActionEvent actionEvent) {
+    }
+}
